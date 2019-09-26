fbcc144d6e9e15a7d5bba31edc6a8afa38c4a928
luke <luke@pcp032042.psi.ch>
2019-09-26 18:41:12 +0200

    get correct pid in deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,13 @@ rsync -e ssh -av ../oai-provider-service $DEPLOY_USER@doi.psi.ch:/tmp/
 ssh $DEPLOY_USER@doi.psi.ch <<'ENDSSH'
 #commands to run on remote host
 sudo -i
-kill -9 `cat oai-provider-service/save_pid.txt`
-rm oai-provider-service/save_pid.txt
+kill -9 `cat oai-provider-service/dist/save_pid.txt`
+rm oai-provider-service/dist/save_pid.txt
 rm -rf oai-provider-service
 mv /tmp/oai-provider-service .
 cd oai-provider-service
-nohup npm start > log.txt 2>&1 &
+cd dist
+NODE_ENV=production
+nohup node index > log.txt 2>&1 &
 echo $! > save_pid.txt
 ENDSSH

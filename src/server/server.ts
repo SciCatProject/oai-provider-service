f4fb815b7c417f311ecc56355cd3c95243333205
Luke Gorman <luke.gorman@psi.ch>
2020-01-20 10:40:46 +0100

    body parser extend max size

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,12 +1,12 @@
 /*
- *  Original work Copyright 2018 Willamette University 
+ *  Original work Copyright 2018 Willamette University
  *  Modified work Copyright 2019 SciCat Organisations
  *
  *  This file is part of OAI-PHM Service.
- *  
+ *
  *  @author Michael Spalti
  *
- *  OAI-PHM Service is based on the Modular OAI-PMH Server, University of Helsinki, 
+ *  OAI-PHM Service is based on the Modular OAI-PMH Server, University of Helsinki,
  *  The National Library of Finland.
  *
  *  OAI-PHM Service is free software: you can redistribute it and/or modify
@@ -15,7 +15,7 @@
  *  (at your option) any later version.
  *
  *  OAI-PHM Service is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
@@ -23,82 +23,87 @@
  *  along with OAI-PHM Service.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import express = require('express');
-import {Application} from 'express';
-import bodyParser = require('body-parser');
-import http = require('http');
-import os = require('os');
-import logger from './logger';
-import {getHostConfiguration, hasHostConfigurationFile} from "./host-config";
+import express = require("express");
+import { Application } from "express";
+import bodyParser = require("body-parser");
+import http = require("http");
+import os = require("os");
+import logger from "./logger";
+import { getHostConfiguration, hasHostConfigurationFile } from "./host-config";
 import * as cors from "cors";
 
 const app = express();
 
-
 export default class ExpressServer {
+  constructor() {
+    const options: cors.CorsOptions = {
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token"
+      ],
+      credentials: true,
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      //origin: API_URL,
+      preflightContinue: false
+    };
 
-    constructor() {
-        const options:cors.CorsOptions = {
-            allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
-            credentials: true,
-            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-            //origin: API_URL,
-            preflightContinue: false
-          };
+    app.use(bodyParser.json());
+    app.use(bodyParser.json({ limit: "50mb" }));
+    app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+    app.use(cors(options));
 
-        app.use(bodyParser.json());
-        app.use(bodyParser({limit: '16mb'}));
-        app.use(bodyParser.urlencoded({extended: true}));
-        app.use(cors(options));
+
+  }
+
+  router(routes: (app: Application) => void): ExpressServer {
+    routes(app);
+    return this;
+  }
+
+  listen(): Application {
+    const config = this.getConfiguration();
+    const port = this.getPort(config);
+
+    const welcome: any = () => {
+      logger.info(
+        `******** Up and running in ${process.env.NODE_ENV ||
+          "development"} @: ${os.hostname()} on port: ${port}}***********`
+      );
+    };
+
+    http.createServer(app).listen(port, welcome());
+    return app;
+  }
+
+  /**
+   * Returns default port if host configuration is not available.
+   * @returns {number}
+   */
+  private getConfiguration(): object {
+    if (hasHostConfigurationFile()) {
+      return getHostConfiguration();
+    } else {
+      logger.warn(
+        "No configuration provided. Using default port. See documentation for details."
+      );
+      return { port: 3000 };
     }
+  }
 
-    router(routes: (app: Application) => void): ExpressServer {
-        routes(app);
-        return this;
+  /**
+   * Return configuration port or default port.
+   * @param configuration
+   * @returns {number}
+   */
+  private getPort(configuration: any): number {
+    if (configuration) {
+      if (configuration.port) {
+        return configuration.port;
+      }
     }
-
-    listen(): Application {
-
-        const config = this.getConfiguration();
-        const port = this.getPort(config);
-
-        const welcome: any = () => {
-            logger.info(`******** Up and running in ${process.env.NODE_ENV || 
-            'development'} @: ${os.hostname() } on port: ${port}}***********`);
-        };
-
-        http.createServer(app).listen(port, welcome());
-        return app;
-    }
-
-    /**
-     * Returns default port if host configuration is not available.
-     * @returns {number}
-     */
-    private getConfiguration(): object {
-
-        if (hasHostConfigurationFile()) {
-            return getHostConfiguration();
-        } else {
-            logger.warn("No configuration provided. Using default port. See documentation for details.");
-            return {port: 3000};
-        }
-
-    }
-
-    /**
-     * Return configuration port or default port.
-     * @param configuration
-     * @returns {number}
-     */
-    private getPort(configuration: any): number {
-        if (configuration) {
-            if (configuration.port) {
-                return configuration.port;
-            }
-        }
-        return 3000;
-    }
-
-
+    return 3000;
+  }
 }
